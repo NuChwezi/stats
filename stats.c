5b002c96c51de40b648f47f8e4fe7e5a620312c8
Joe Willrich <joewillrich@gmail.com>
2019-02-07 12:46:13 +0300

    just getting started

---
+++ stats.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Welcome. U can compute the following:\n[m]ean , [v]ariance, [s]tandard deviation\nFor the list of numbers supplied via stdin (comma separated)\n");
+
+    return 0;
+}
